5a7fd08146a51d41c0798b6bdc5c2cd355f5bfae
Dmitry Sedykh <sedykh@gmail.com>
2016-10-10 21:07:08 +0300

    rest v6.0

--- http.go
+++ http.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/mdigger/log"
-	"github.com/mdigger/rest"
+	"github.com/mdigger/rest3"
 )
 
 type Service struct {
@@ -20,15 +19,11 @@ func NewService(config *Config) *Service {
 	// инициализируем мультиплексор HTTP-запросов
 	var mux = &rest.ServeMux{
 		Headers: map[string]string{
+			"Server":            "Pusher/2.0",
 			"X-API-Version":     "1.1",
 			"X-Service-Version": version,
 		},
-		Logger:     log.Default,
-		SendErrors: true,
-		Options: &rest.Options{
-			DataAdapter: rest.Adapter,
-			Encoder:     rest.JSONEncoder(true),
-		},
+		Logger: log.Default,
 	}
 	var service = &Service{
 		mux:    mux,
@@ -50,51 +45,51 @@ func NewService(config *Config) *Service {
 
 // AdminAuth проверяет авторизацию администратора. Возвращает 0, nil, если
 // администратор успешно авторизован.
-func (s *Service) AdminAuth(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) AdminAuth(c *rest.Context) error {
 	if !s.config.IsAdminAuthorization() {
-		return 0, nil // авторизация не требуется
+		return nil // авторизация не требуется
 	}
 	// разбираем заголовок с авторизацией
-	login, password, ok := r.BasicAuth()
+	login, password, ok := c.BasicAuth()
 	if !ok {
 		realm := fmt.Sprintf("Basic realm=%s admin", appName)
-		w.Header().Set("WWW-Authenticate", realm)
-		return http.StatusUnauthorized, errors.New("no admin authorization")
+		c.SetHeader("WWW-Authenticate", realm)
+		return rest.ErrUnauthorized
 	}
 	// проверяем авторизацию
 	if !s.config.AdminAuthorization(login, password) {
-		return http.StatusForbidden, errors.New("bad admin authorization")
+		return rest.ErrForbidden
 	}
-	return 0, nil // администратор авторизован
+	return nil // администратор авторизован
 }
 
 // GetUsers отдает список пользователей для авторизации.
-func (s *Service) GetUsers(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) GetUsers(c *rest.Context) error {
 	// проверяем авторизацию администратора
-	if code, err := s.AdminAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.AdminAuth(c); err != nil {
+		return err
 	}
 	// отдаем список пользователей
-	return rest.Write(w, r, 200, rest.JSON{"users": s.config.UsersList()})
+	return c.Write(rest.JSON{"users": s.config.UsersList()})
 }
 
 // AddUser регистрирует нового пользователя для авторизации обращения к сервису.
-func (s *Service) AddUser(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) AddUser(c *rest.Context) error {
 	// проверяем авторизацию администратора
-	if code, err := s.AdminAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.AdminAuth(c); err != nil {
+		return err
 	}
 	// разбираем информацию о пользователе из запроса
 	var user = new(struct {
 		Login    string `json:"login" form:"login"`
 		Password string `json:"password" form:"password"`
 	})
-	err := rest.Bind(r, user)
+	err := c.Bind(user)
 	if err != nil {
-		return http.StatusBadRequest, err
+		return err
 	}
 	if user.Login == "" {
-		return http.StatusBadRequest, errors.New("empty login")
+		return rest.ErrBadRequest
 	}
 	// добавляем информацию о пользователе
 	exist := s.config.AddUser(user.Login, user.Password)
@@ -103,42 +98,43 @@ func (s *Service) AddUser(w http.ResponseWriter, r *http.Request) (int, error) {
 	if !exist {
 		code = http.StatusCreated
 	}
+	c.SetStatus(code)
 	// отдаем список пользователей
-	return rest.Write(w, r, code, rest.JSON{"users": s.config.UsersList()})
+	return c.Write(rest.JSON{"users": s.config.UsersList()})
 }
 
 // RemoveUser удаляет пользователя из списка авторизации.
-func (s *Service) RemoveUser(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) RemoveUser(c *rest.Context) error {
 	// проверяем авторизацию администратора
-	if code, err := s.AdminAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.AdminAuth(c); err != nil {
+		return err
 	}
 	// получаем логин из запроса пути
-	var login = rest.Params(r).Get("login")
+	var login = c.Param("login")
 	// удаляем пользователя из списка
 	exist := s.config.RemoveUser(login)
 	if !exist {
-		return http.StatusNotFound, fmt.Errorf("user %s not registered", login)
+		return c.Error(http.StatusNotFound, fmt.Sprintf("user %s not registered", login))
 	}
 	// отдаем список пользователей
-	return rest.Write(w, r, http.StatusOK, rest.JSON{"users": s.config.UsersList()})
+	return c.Write(rest.JSON{"users": s.config.UsersList()})
 }
 
 // ChangeUser изменяет пароль пользователя для авторизации.
-func (s *Service) ChangeUser(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) ChangeUser(c *rest.Context) error {
 	// проверяем авторизацию администратора
-	if code, err := s.AdminAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.AdminAuth(c); err != nil {
+		return err
 	}
 	// получаем логин из запроса пути
-	var login = rest.Params(r).Get("login")
+	var login = c.Param("login")
 	// получаем пароль из запроса
 	var password = new(struct {
 		Password string `json:"password" form:"password"`
 	})
-	err := rest.Bind(r, password)
+	err := c.Bind(password)
 	if err != nil {
-		return http.StatusBadRequest, err
+		return err
 	}
 	// изменяем пароль пользователя
 	exist := s.config.AddUser(login, password.Password)
@@ -146,121 +142,120 @@ func (s *Service) ChangeUser(w http.ResponseWriter, r *http.Request) (int, error
 	if !exist {
 		code = http.StatusCreated
 	}
+	c.SetStatus(code)
 	// отдаем список пользователей
-	return rest.Write(w, r, code, rest.JSON{"users": s.config.UsersList()})
+	return c.Write(rest.JSON{"users": s.config.UsersList()})
 }
 
 // UserAuth проверяет авторизацию пользователя. Возвращает 0, nil, если
 // пользователь успешно авторизован или авторизация не требуется.
-func (s *Service) UserAuth(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) UserAuth(c *rest.Context) error {
 	if !s.config.IsUserAuthorization() {
-		return 0, nil // авторизация не требуется
+		return nil // авторизация не требуется
 	}
 	// разбираем заголовок с авторизацией
-	login, password, ok := r.BasicAuth()
+	login, password, ok := c.BasicAuth()
 	if !ok {
 		realm := fmt.Sprintf("Basic realm=%s", appName)
-		w.Header().Set("WWW-Authenticate", realm)
-		return http.StatusUnauthorized, errors.New("no user authorization")
+		c.SetHeader("WWW-Authenticate", realm)
+		return rest.ErrUnauthorized
 	}
 	// проверяем авторизацию
 	if !s.config.UserAuthorization(login, password) {
-		return http.StatusForbidden, errors.New("bad user authorization")
+		return rest.ErrForbidden
 	}
-	return 0, nil // администратор авторизован
+	return nil // администратор авторизован
 }
 
 // GetTokens отдает список зарегистрированных токенов пользователя
-func (s *Service) GetTokens(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) GetTokens(c *rest.Context) error {
 	// проверяем авторизацию пользователя
-	if code, err := s.UserAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.UserAuth(c); err != nil {
+		return err
 	}
-	params := rest.Params(r)    // параметры пути
-	user := params.Get("login") // логин пользователя
+	user := c.Param("login") // логин пользователя
 	if user == "" {
-		return http.StatusNotFound, errors.New("empty user login")
+		return rest.ErrNotFound
 	}
-	topic := params.Get("topic") // тема
+	topic := c.Param("topic") // тема
 	if topic == "" {
-		return http.StatusNotFound, errors.New("empty topic")
+		return rest.ErrNotFound
 	}
-	query := r.URL.Query()                // разобранные параметры запроса
+	query := c.Request.URL.Query()        // разобранные параметры запроса
 	sandbox := len(query["sandbox"]) != 0 // флаг sandbox
 	// запрашиваем список токенов пользователя
 	tokens, err := s.config.Store.GetUserTopicTokens(topic, sandbox, user)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
 	if len(tokens) == 0 {
-		return http.StatusNotFound,
-			fmt.Errorf("tokens for user %s not registered", user)
+		return c.Error(http.StatusNotFound,
+			fmt.Sprintf("tokens for user %s not registered", user))
 	}
-	return rest.Write(w, r, http.StatusOK, rest.JSON{"tokens": tokens})
+	return c.Write(rest.JSON{"tokens": tokens})
 }
 
 // AddToken регистрирует токен пользовательского устройства.
-func (s *Service) AddToken(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) AddToken(c *rest.Context) error {
 	// проверяем авторизацию пользователя
-	if code, err := s.UserAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.UserAuth(c); err != nil {
+		return err
 	}
-	params := rest.Params(r)    // параметры пути
-	user := params.Get("login") // логин пользователя
+	user := c.Param("login") // логин пользователя
 	if user == "" {
-		return http.StatusNotFound, errors.New("empty user login")
+		return rest.ErrNotFound
 	}
-	topic := params.Get("topic") // тема
+	topic := c.Param("topic") // тема
 	if topic == "" {
-		return http.StatusNotFound, errors.New("empty topic")
+		return c.Error(http.StatusNotFound, "empty topic")
 	}
-	query := r.URL.Query()                // разобранные параметры запроса
+	query := c.Request.URL.Query()        // разобранные параметры запроса
 	sandbox := len(query["sandbox"]) != 0 // флаг sandbox
 	var token = new(struct {
 		Token string `json:"token" form:"token"`
 	})
-	err := rest.Bind(r, token)
+	err := c.Bind(token)
 	if err != nil {
-		return http.StatusBadRequest, err
+		return err
 	}
 	// сохраняем в хранилище токенов устройств
 	err = s.config.AddToken(user, topic, token.Token, sandbox)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
 	// запрашиваем список токенов пользователя
 	tokens, err := s.config.Store.GetUserTopicTokens(topic, sandbox, user)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
-	return rest.Write(w, r, http.StatusCreated, rest.JSON{"tokens": tokens})
+	c.SetStatus(http.StatusCreated)
+	return c.Write(rest.JSON{"tokens": tokens})
 }
 
 // PushUser отправляет push-уведомления на все устройства пользователя.
-func (s *Service) PushUser(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) PushUser(c *rest.Context) error {
 	// проверяем авторизацию пользователя
-	if code, err := s.UserAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.UserAuth(c); err != nil {
+		return err
 	}
-	params := rest.Params(r)    // параметры пути
-	user := params.Get("login") // логин пользователя
+	user := c.Param("login") // логин пользователя
 	if user == "" {
-		return http.StatusNotFound, errors.New("empty user login")
+		return rest.ErrNotFound
 	}
-	topic := params.Get("topic") // тема
+	topic := c.Param("topic") // тема
 	if topic == "" {
-		return http.StatusNotFound, errors.New("empty topic")
+		return c.Error(http.StatusNotFound, "empty topic")
 	}
-	query := r.URL.Query()                // разобранные параметры запроса
+	query := c.Request.URL.Query()        // разобранные параметры запроса
 	sandbox := len(query["sandbox"]) != 0 // флаг sandbox
 	// получаем список токенов пользователя
 	tokens, err := s.config.Store.GetUserTopicTokens(topic, sandbox, user)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
 	if len(tokens) == 0 {
-		return http.StatusNotFound,
-			fmt.Errorf("tokens for user %s not registered", user)
+		return c.Error(http.StatusNotFound,
+			fmt.Sprintf("tokens for user %s not registered", user))
 	}
 	// разбираем запроса для отправки уведомления
 	var notification = new(struct {
@@ -269,12 +264,12 @@ func (s *Service) PushUser(w http.ResponseWriter, r *http.Request) (int, error) 
 		LowPriority bool                   `json:"lowPriority" form:"lowPriority"`
 		CollapseID  string                 `json:"collapseId" form:"collapseId"`
 	})
-	err = rest.Bind(r, notification)
+	err = c.Bind(notification)
 	if err != nil {
-		return http.StatusBadRequest, err
+		return err
 	}
 	if len(notification.Payload) == 0 {
-		return http.StatusBadRequest, errors.New("empty payload")
+		return c.Error(http.StatusBadRequest, "empty payload")
 	}
 	// формируем данные для уведомления
 	var n = Notification{
@@ -289,25 +284,24 @@ func (s *Service) PushUser(w http.ResponseWriter, r *http.Request) (int, error) 
 	// отправляем на все токены пользователя
 	sent, err := s.config.Push(n, tokens)
 	if err != nil {
-		return http.StatusBadRequest, err
+		return err
 	}
 	// отдаем количество отправленных сообщений
-	return rest.Write(w, r, http.StatusOK, rest.JSON{"sent": sent})
+	return c.Write(rest.JSON{"sent": sent})
 }
 
 // Push отправляет push-уведомления на все устройства указанных в запросе
 // пользователей.
-func (s *Service) Push(w http.ResponseWriter, r *http.Request) (int, error) {
+func (s *Service) Push(c *rest.Context) error {
 	// проверяем авторизацию пользователя
-	if code, err := s.UserAuth(w, r); code != 0 || err != nil {
-		return code, err
+	if err := s.UserAuth(c); err != nil {
+		return err
 	}
-	params := rest.Params(r)     // параметры пути
-	topic := params.Get("topic") // тема
+	topic := c.Param("topic") // тема
 	if topic == "" {
-		return http.StatusNotFound, errors.New("empty topic")
+		return c.Error(http.StatusNotFound, "empty topic")
 	}
-	query := r.URL.Query()                // разобранные параметры запроса
+	query := c.Request.URL.Query()        // разобранные параметры запроса
 	sandbox := len(query["sandbox"]) != 0 // флаг sandbox
 	// разбираем запроса для отправки уведомления
 	var notification = new(struct {
@@ -317,25 +311,25 @@ func (s *Service) Push(w http.ResponseWriter, r *http.Request) (int, error) {
 		CollapseID  string                 `json:"collapseId" form:"collapseId"`
 		Users       []string               `json:"users" form:"user"`
 	})
-	err := rest.Bind(r, notification)
+	err := c.Bind(notification)
 	if err != nil {
-		return http.StatusBadRequest, err
+		return err
 	}
 
 	if len(notification.Payload) == 0 {
-		return http.StatusBadRequest, errors.New("empty payload")
+		return c.Error(http.StatusBadRequest, "empty payload")
 	}
 	if len(notification.Users) == 0 {
-		return http.StatusBadRequest, errors.New("empty users list")
+		return c.Error(http.StatusBadRequest, "empty users list")
 	}
 	// получаем список токенов пользователя
 	tokens, err := s.config.Store.GetUserTopicTokens(topic, sandbox,
 		notification.Users...)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
 	if len(tokens) == 0 {
-		return http.StatusNotFound, errors.New("tokens not registered")
+		return c.Error(http.StatusNotFound, "tokens not registered")
 	}
 	// формируем данные для уведомления
 	var n = Notification{
@@ -349,8 +343,8 @@ func (s *Service) Push(w http.ResponseWriter, r *http.Request) (int, error) {
 	// отправляем на все токены пользователя
 	sent, err := s.config.Push(n, tokens)
 	if err != nil {
-		return http.StatusBadRequest, err
+		return err
 	}
 	// отдаем количество отправленных сообщений
-	return rest.Write(w, r, http.StatusOK, rest.JSON{"sent": sent})
+	return c.Write(rest.JSON{"sent": sent})
 }
